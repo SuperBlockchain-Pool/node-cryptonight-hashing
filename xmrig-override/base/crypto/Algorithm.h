97be5819a16dc0051b17c2b43a0dd073da9d59f3
SuperBlockchain Pool <55582476+SuperBlockchain-Pool@users.noreply.github.com>
2024-09-09 20:50:13 +0200

    XEQ

--- xmrig-override/base/crypto/Algorithm.h
+++ xmrig-override/base/crypto/Algorithm.h
@@ -90,6 +90,7 @@ public:
         RX_SFX          = 0x72151273,   // "rx/sfx"           RandomSFX (Safex Cash).
         RX_KEVA         = 0x7214116b,   // "rx/keva"          RandomKEVA (Keva).
         RX_NEVO         = 0x7214126e,   // "rx/nevo"          RandomNEVO (Nevocoin).
+        RX_XEQ          = 0x72121000,   // "rx/xeq            RandomXEQ (Equilibria)
         AR2_CHUKWA      = 0x61130000,   // "argon2/chukwa"    Argon2id (Chukwa).
         AR2_CHUKWA_V2   = 0x61140000,   // "argon2/chukwav2"  Argon2id (Chukwa v2).
         AR2_WRKZ        = 0x61120000,   // "argon2/wrkz"      Argon2id (WRKZ)
